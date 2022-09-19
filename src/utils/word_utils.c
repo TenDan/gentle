1b71ebd1aa644994814db1bd200908647ab912f4
TenDan <maciej.slusarz@proton.me>
2022-09-19 12:00:10 +0200

    Add word_utils.h and add bin file to .gitignore

---
+++ src/utils/word_utils.c
@@ -0,0 +1,6 @@
+#include "word_utils.h"
+
+void firstLetterUppercased(word_arr_ptr word) 
+{
+
+}
---
+++ src/utils/word_utils.h
@@ -0,0 +1,10 @@
+#ifndef WORD_UTILS_H
+#define WORD_UTILS_H
+
+#include "letter_utils.h"
+
+typedef char** word_arr_ptr;
+
+void firstLetterUppercased(word_arr_ptr word);
+
+#endif
