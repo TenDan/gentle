17fbfb429dd3da840ab2bfb5b454c07a8963138d
TenDan <maciej.slusarz@proton.me>
2022-09-21 18:59:05 +0200

    Add sentence_utils

---
+++ src/utils/sentence_utils.c
@@ -0,0 +1,13 @@
+#include "sentence_utils.h"
+#include "word_utils.h"
+
+void gentleSentence(sentence_ptr sentence)
+{
+    char *found;
+    while ((found = strsep(sentence, " ")) != NULL)
+    {
+        firstLetterUppercased(found);
+        printf("%s ", found);
+    }
+    printf("\n");
+}
---
+++ src/utils/sentence_utils.h
@@ -0,0 +1,10 @@
+#ifndef SENTENCE_UTILS_H
+#define SENTENCE_UTILS_H
+
+#include <stdio.h>
+
+typedef char** sentence_ptr;
+
+void gentleSentence(sentence_ptr sentence);
+
+#endif
